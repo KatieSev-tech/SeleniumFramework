0a1b0458f75ee5d03e98816adb2e79be68402b21
KatieSev-tech <katie.sevriukova@gmail.com>
2023-06-28 12:43:53 -0700

    AddedMathOperations

--- src/main/java/MainJavaBasicsHomeWork.java
+++ src/main/java/MainJavaBasicsHomeWork.java
@@ -8,11 +8,19 @@ public class MainJavaBasicsHomeWork {
         String productName = "book";
         int age = 24;
         double price = 21.99;
+        int ten = 10;
+        int five = 5;
+        double twentyhalf = 20.5;
 
         System.out.println("Dear " + firstName + " " + lastName + "! Welcome to our book store " + storeName + ". " +
                 "Our data indicates that today you turn " + age + " years old. We offer you special gift! " +
                 "With purchase on any book above $" + price + ", you will get additional " + productName + " for free. " +
                 "Offer is available in all the local stores around " + locationState + " state.");
+        System.out.println(ten * five);
+        System.out.println(ten / five);
+        System.out.println(ten + five);
+        System.out.println(ten - five);
+        System.out.println(twentyhalf - five);
 
     }
 
