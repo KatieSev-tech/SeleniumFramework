663f8680e8efa427a699ae562e7431a38a659e50
KatieSev-tech <katie.sevriukova@gmail.com>
2023-07-27 00:00:34 -0700

    update baseMain

--- src/main/java/pageobjects/BaseMain.java
+++ src/main/java/pageobjects/BaseMain.java
@@ -8,6 +8,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BaseMain {
+<<<<<<< Updated upstream
+=======
+
+    String menuList = "//body[1]/div[1]/div[1]/div[1]/div[2]/a";
+    int listSizeActual;
+    int listSizeExpected = 4;
+    String expectedTitle = "My Fork - your educational solution";
+    String actualTitle;
+    String socialsMenuList = "//body/div[@id='app']/div[3]/div[1]/a";
+    int listSocialsListExpected = 3;
+    int listSocialsListActual;
+    int pixels;
+
+
+>>>>>>> Stashed changes
     public ChromeDriver driver;
 
     public BaseMain(ChromeDriver driver) {
@@ -22,10 +37,26 @@ public class BaseMain {
         }
     }
 
+<<<<<<< Updated upstream
     public void scrollingPage (){
         int pixels = 3000;
         JavascriptExecutor js = (JavascriptExecutor) driver;
         js.executeScript("window.scrollBy(0," + pixels + ")", "");
     }
+=======
+    public void windowHandling () {
+        List<String> tabHandler = new ArrayList<>(driver.getWindowHandles());
+        int tab = tabHandler.size();
+        driver.switchTo().window(tabHandler.get(tab));
+        driver.navigate().to(tabHandler.get(tab));
+    }
+
+
+        public void scrollingPage () {
+
+            JavascriptExecutor js = (JavascriptExecutor) driver;
+            js.executeScript("window.scrollBy(0," + pixels + ")", "");
+        }
+>>>>>>> Stashed changes
 }
 
